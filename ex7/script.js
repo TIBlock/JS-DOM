dc89493cb5281b916dc856da56e4a880971c3b62
Taylor Blocker <taylorblocker@gmail.com>
2019-08-02 22:32:53 -0500

    completed exercise 7, i am able to toggle the text but i am uncertain if it is exactly how they want it to appear.

--- ex7/script.js
+++ ex7/script.js
@@ -1,3 +1,17 @@
 // Make the section disappear and reappear whenever you click the section-header
 // BONUS: Can you make the section slide up and down like this? :
 // https://www.w3schools.com/bootstrap/bootstrap_collapse.asp
+
+
+var sect = document.querySelector("#section");
+console.log(sect);
+var sectHeader = document.querySelector("#section-header")
+console.log(sectHeader);
+
+
+sectHeader.addEventListener('click', function(){
+
+    sect.toggleAttribute('hidden');
+
+
+});
